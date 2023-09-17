699a410da114fe912d7c8a3fcb085d04db992789
Ivan Marchand <marchandivan@gmail.com>
2023-09-17 18:12:12 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libopencv-dev qtbase5-dev python3-pyqt5 python3-opencv
+pip install pyinstaller
+mkdir ~/.pirobot-remote/
+cp keyboard.config.json ~/.pirobot-remote/
+pyinstaller -n piremote -F --add-data ./config:config --add-data ./pics:pics main.py
